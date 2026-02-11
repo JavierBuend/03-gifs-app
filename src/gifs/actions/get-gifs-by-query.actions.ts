057894c67147865f0626ea7a5ba63abf7c951e8b
Javier <186595036+cepex1@users.noreply.github.com>
2026-02-11 23:44:46 +0100

    comentarios de  código

--- src/gifs/actions/get-gifs-by-query.actions.ts
+++ src/gifs/actions/get-gifs-by-query.actions.ts
@@ -3,19 +3,28 @@ import { giphyApi } from '../api/giphy.api';
 import type { GiphyResponse } from '../interfaces/giphy.response';
 import type { Gif } from '../interfaces/gif.interface';
 
+// Función asíncrona que recibe un texto de búsqueda (query)
+// y devuelve una promesa que resuelve en un arreglo de Gif
 export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
+
+  // Realiza una petición GET al endpoint '/search'
+  // Se tipa la respuesta como GiphyResponse para tener autocompletado y seguridad de tipos
+  
   const response = await giphyApi<GiphyResponse>('/search', {
     params: {
-      q: query,
-      limit: 10,
+      q: query,   // Texto que se enviará como término de búsqueda
+      limit: 10,  // Limita la respuesta a 10 resultados
     },
   });
+
+  // Transforma la respuesta original de la API
+  // en un arreglo con la estructura definida por la interfaz Gif
 
   return response.data.data.map((gif) => ({
     id: gif.id,
     title: gif.title,
-    url: gif.images.original.url,
-    width: Number(gif.images.original.width),
+    url: gif.images.original.url, 
+    width: Number(gif.images.original.width), 
     height: Number(gif.images.original.height),
   }));
 };
